207b12c0a3484ef0c98b3199fbd8e8cb34ddcec2
MohammedIslam24 <mohammedislamghomrani2019@gmail.co>
2025-06-15 14:54:30 +0100

    enhancement a little bit

--- calc.cpp
+++ calc.cpp
@@ -3,8 +3,25 @@ using namespace std;
 
 int main() {
     int a, b;
+    char op;
     cout << "أدخل رقمين: ";
     cin >> a >> b;
-    cout << "المجموع: " << a + b << endl;
+    cout << "اختر عملية (+ - * /): ";
+    cin >> op;
+
+    switch(op) {
+        case '+': cout << "النتيجة: " << a + b; break;
+        case '-': cout << "النتيجة: " << a - b; break;
+        case '*': cout << "النتيجة: " << a * b; break;
+        case '/': 
+            if (b != 0)
+                cout << "النتيجة: " << a / b;
+            else
+                cout << "لا يمكن القسمة على صفر";
+            break;
+        default: cout << "عملية غير صالحة!";
+    }
+
+    cout << endl;
     return 0;
 }
